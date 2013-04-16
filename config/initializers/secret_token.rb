36319c5d8c7d01cf1584c122d907ef086e10c64a
Kenny Duong <keduong@gmail.com>
2013-04-16 13:31:24 -0700

    rails new moonrise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moonrise::Application.config.secret_token = 'd002c0da44b4d0fb9f872963153ef7eb0686cc804bab031ef3eaf911696d82801b078e4ebb35c8d93f38159e10093856356f768c850943b082522236a48e0676'
