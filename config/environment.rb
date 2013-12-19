2b2f3b0a5528011e2ac9061bf97375bd4c11a6e7
Kenny Duong <keduong@gmail.com>
2013-12-18 19:48:07 -0800

    email settings

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,15 @@ require File.expand_path('../application', __FILE__)
 Moonrise::Application.initialize!
 
 
-Moonrise::Application.configure do config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = { 
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "minkmeadow.com", 
-  authentication: "plain",
-  user_name: ENV['GMAIL_USERNAME'], 
-  password: ENV['GMAIL_PASSWORD'], 
-  enable_starttls_auto: true
-}
+Moonrise::Application.configure do 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { 
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "minkmeadow.com", 
+    authentication: "plain",
+    user_name: ENV['GMAIL_USERNAME'], 
+    password: ENV['GMAIL_PASSWORD'], 
+    enable_starttls_auto: true
+  }
 end
